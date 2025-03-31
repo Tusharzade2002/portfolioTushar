724dd63fc430d5aec610c2938003eb1972af3768
Tusharzade <tusharzade29@gmail.com>
2025-03-31 21:30:59 +0530

    Footer Designed

--- portfolio/src/Layout/Footer.jsx
+++ portfolio/src/Layout/Footer.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import { IoLogoInstagram } from "react-icons/io5";
+import { FaFacebook } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa6";
+import { FaGoogle } from "react-icons/fa6";
+import {Link} from 'react-router-dom'
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='py-4'>
+         <div class="flex items-center justify-center">
+         <IoLogoInstagram  className='mx-4'/>
+         <FaFacebook className='mx-4'/>
+         <FaTwitter className='mx-4'/>
+         <FaGoogle className='mx-4'/>
+         </div>
+         <div class="flex items-center justify-center mt-4">
+            <Link className='mx-6'>Home</Link>
+            <Link className='mx-6'>Work</Link>
+            <Link className='mx-6'>Service</Link>
+            <Link className='mx-6'>Contact</Link>
+         </div>
+    </div>
   )
 }
 
