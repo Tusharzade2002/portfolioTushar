7256968204b038d17d1cca4ba15b625ba83e7974
Tusharzade <tusharzade29@gmail.com>
2025-03-31 17:18:55 +0530

    icon add

--- portfolio/src/Pages/Contact.jsx
+++ portfolio/src/Pages/Contact.jsx
@@ -1,39 +1,51 @@
 import React from 'react'
-
+import { MdAddIcCall } from "react-icons/md";
+import { MdEmail } from "react-icons/md";
+import { IoLocation } from "react-icons/io5";
 function Contact() {
   return (
     <div>
-        <h1>Contact <p>Me</p></h1>
+        <h1 class="text-center font-black text-5xl my-4 ">Contact Me</h1>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni ipsam in nesciunt impedit eius iste accusamus fuga ab officiis eligendi.</p>
-        <div>
-            <div>
-              <div>
-                   <img src='' alt=''/>
+        <div class="flex flex-col">
+          <div>
+            <div class="flex items-center">
+              <div class="w-5 h-5 rounded-xl align-center bg-indigo-600" >
+                  <MdAddIcCall />
               </div>
               <div>
                        <p>Call Me</p>
                        <p>91+ 0987654321</p>
                </div>
             </div>
-            <div>
+            <div class="flex items-center">
               <div>
-                   <img src='' alt=''/>
+                       <MdEmail />         
               </div>
               <div>
-                       <p>Call Me</p>
-                       <p>91+ 0987654321</p>
+                
+                       <p>Email</p>
+                       <p>tusharzade29@gmail.com</p>
                </div>
             </div>
-            <div>
+            <div class="flex items-center">
               <div>
-                   <img src='' alt=''/>
+                     <IoLocation />
               </div>
               <div>
-                       <p>Call Me</p>
-                       <p>91+ 0987654321</p>
+                  <p>Address</p>
+                  <p>Butibori ,Nagpur</p>
+                     
                </div>
             </div>
+           </div>
+           <div>
+                <input className='m-3' type='text'/>
+                <input className='m-3' type='text'/>
+                <input className='m-3' type='text'/>
+           </div>
         </div>
+
     </div>
   )
 }
