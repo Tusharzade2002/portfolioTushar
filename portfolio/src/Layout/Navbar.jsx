d1914df009378a7afdd2d405763ad8079ee010e6
Tusharzade <tusharzade29@gmail.com>
2025-04-04 13:11:51 +0530

    logo design

--- portfolio/src/Layout/Navbar.jsx
+++ portfolio/src/Layout/Navbar.jsx
@@ -43,11 +43,9 @@ function Navbar() {
       </div>
       <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
         <div class="flex shrink-0 items-center">
-         <button className=' flex transition-transform duration-300 transform rounded-full bg-gradient-to-r from-green-400 to-blue-500 md:inline hover:scale-110 animate-slide-in-delay p-2 text-zinc-50 items-center'>  
-              <div className='flex items-center'>
-                      <FaDownload  className='me-2'/>
-                      <a href='https://drive.google.com/uc?export=download&id=1o-6xI157ibUk8l6Nvh8SAw9442NLhR7L'> Download CV</a>
-              </div>
+          <p className='text-white font-semibold text-2xl'>T Z</p>
+         <button className='  mt-3 flex transition-transform duration-300 transform rounded-full bg-gradient-to-r from-green-400 to-blue-500 md:inline hover:scale-110 animate-slide-in-delay p-1 text-zinc-50 items-center'>  
+           
         </button>
         </div>
         <div class="hidden sm:ml-6 sm:block">
@@ -60,13 +58,7 @@ function Navbar() {
         </div>
       </div>
       <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-        <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-          <span class="absolute -inset-1.5"></span>
-          <span class="sr-only">View notifications</span>
-          <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-          </svg>
-        </button>
+       
 
         {/* <!-- Profile dropdown --> */}
         <div class="relative ml-3">
