84e4dfc971ee138341ee584be398c2759337192d
Tusharzade <tusharzade29@gmail.com>
2025-04-25 20:06:41 +0530

    add ecommerce project

--- portfolio/src/Pages/Service.jsx
+++ portfolio/src/Pages/Service.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {CalendarDays} from 'lucide-react';
 import projectimg from '../assets/Screenshot 2025-04-08 113842.png'
-
+import ecommerceproject from "../assets/ecommerceproject.png"
 function Service() {
   return (
     <div style={{backgroundColor:"#042c30"}}>
@@ -15,11 +15,18 @@ function Service() {
                   <div className='flex text-teal-300'>
                          <img className='rounded-xl' src={projectimg}/>
                   </div>
-                      <div className='text-white'>
-                          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, placeat? Iusto minus, dolores ea unde deserunt provident officiis sunt accusamus!
+                      <div className='text-white text-xl'>
+                          Quick Notes Application Create by using react js aur data are stored in the local Storage.
                       </div>
                 </div> 
-
+                <div className=' content w-[500px] text-black p-5 m-7 '>
+                  <div className='flex text-teal-300'>
+                         <img className='rounded-xl w-full object-fill'  src={ecommerceproject}/> 
+                  </div>
+                      <div className='text-white text-xl'>
+                          E-commerce Application Created by using React js in Frontend and Backend By using node.js , DataBase Used as MongoDB.
+                      </div>
+                </div>  
                           
            </div> 
 </div>
